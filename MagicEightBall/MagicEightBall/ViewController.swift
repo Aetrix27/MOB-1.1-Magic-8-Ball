28e278a373a3c59849402c53355e05f72506766e
Aetrix27 <davedogg323@gmail.com>
2020-11-20 00:14:44 -0800

    Updated UI

---
+++ MagicEightBall/MagicEightBall/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MagicEightBall
+//
+//  Created by David Guerrero on 11/19/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
